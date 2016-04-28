fcffb535559f8c7b27de21f9320b1cb08df6b763
C0deZLee <c0dezlee@gmail.com>
2016-04-28 12:59:21 -0400

    fix bug

--- mapreduce.c
+++ mapreduce.c
@@ -205,12 +205,12 @@ mr_start(struct map_reduce *mr, const char *path, const char *ip, uint16_t port)
     //http://www.binarytides.com/multiple-socket-connections-fdset-select-linux/
     // Connect all the clients
 
-    while((mr->client_sockfd[i] = accept(mr->server_sockfd, (struct sockaddr *)&mr->client_addr[i], &mr->client_addr_length) >= 0){
-      socket_counter++;
-      if (mr->client_sockfd[i] < 0) {
+    while((mr->client_sockfd[socket_counter] = accept(mr->server_sockfd, (struct sockaddr *)&mr->client_addr[socket_counter], &mr->client_addr_length) >= 0){
+      if (mr->client_sockfd[socket_counter] < 0) {
         printf("Server: Cannot connect client %d.\n", i);
         return -1;
       }
+      socket_counter++;
       if(socket_counter == mr->n_threads){
         printf("Server: All clients connected!\n");
         break;
