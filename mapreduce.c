79f80a8909af53d5b2192d9f2454b85694e16d77
C0deZLee <c0dezlee@gmail.com>
2016-04-28 21:41:48 -0400

    add break in the reduce wrapper

--- mapreduce.c
+++ mapreduce.c
@@ -63,6 +63,8 @@ static void *reduce_wrapper(void* reduce_args) {
       return NULL;
     }
     socket_counter++;
+
+    if(socket_counter == args->mr->n_threads - 1) break;
   }
 
   printf("Server: All clients connected!\n");
